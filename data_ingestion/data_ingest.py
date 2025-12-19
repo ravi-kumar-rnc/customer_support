502a4890df9fc3543c78eec32925e035893dc063
ravi-2-kumar <ravi.2.kumar@atos.net>
2025-12-19 14:01:16 +0530

    Updating the data transformation

--- data_ingestion/data_ingest.py
+++ data_ingestion/data_ingest.py
@@ -3,3 +3,16 @@ from dotenv import load_dotenv
 import os
 import pandas as pd
 from data_ingestion.data_transform import data_converter
+
+
+class ingest_data:
+    def __init__(self):
+        print("Data Ingestion class initialized")
+
+    def data_ingestion(self):
+        pass
+
+
+
+if __name__ == "__main__":
+    data_ingestion = ingest_data()
--- data_ingestion/data_transform.py
+++ data_ingestion/data_transform.py
@@ -4,7 +4,31 @@ from langchain_core.documents import Document
 
 class data_converter:
     def __init__(self):
-        pass
+        print("Data Transformation class initialized")
+        self.product_data = pd.read_csv("C:\\customer_support\\data\\flipkart_product_review.csv")
+        #print(self.product_data.head())
 
     def data_transformation(self):
-        pass
+        required_columns = self.product_data.columns[1:]  
+        pdt_list = []      
+        for index, row in self.product_data.iterrows():
+            object = {"product_name": row["product_title"],
+                      "rating": row["rating"],
+                      "summary": row["summary"],
+                        "review": row["review"]}
+            pdt_list.append(object)
+            #print(pdt_list[0])
+
+        docs = []
+        for dtls in pdt_list:
+            metadata = {"product_name": dtls["product_name"], 
+                        "rating": dtls["rating"], 
+                        "summary": dtls["summary"]}
+            doc = Document(page_content=dtls["review"], metadata=metadata)
+            docs.append(doc)
+        print(docs[0])
+        return docs
+
+if __name__=="__main__":
+    data_conv = data_converter()
+    data_conv.data_transformation()
